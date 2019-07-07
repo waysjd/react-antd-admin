67b4fab21bc979c90cdfd261e82704567ad17467
jdways <1229303219@qq.com>
2019-07-07 22:45:56 +0800

    第一次构建版本

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from 'antd/es/button';
+import logo from './assets/svg/logo.svg';
+import './style/App.css';
 
 const App: React.FC = () => {
   return (
@@ -18,6 +19,7 @@ const App: React.FC = () => {
         >
           Learn React
         </a>
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
