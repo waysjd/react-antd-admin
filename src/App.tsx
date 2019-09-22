f35b95e24038a270a471d408a8b7c09bba614400
jdways <1229303219@qq.com>
2019-09-22 16:42:49 +0800

    提交分支信息

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import Button from 'antd/es/button';
-import logo from './assets/svg/logo.svg';
+import MainLayout from './layout/index'
+// import Button from 'antd/es/button';
+// import logo from './assets/svg/logo.svg';
 import './style/App.css';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button type="primary">Button</Button>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.tsx</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <Button type="primary">Button</Button>
+    //   </header>
+    // </div>
+    <div className="app-wrapper">
+      <MainLayout/>
     </div>
   );
 }
---
+++ src/components/Table/index.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+class Table extends React.Component{
+
+}
+
+export default Table 
